193652308ee4e1b6995feb11801561d49967e2a0
tanhaoan333 <tanhaoan@huawei.com>
2026-06-02 17:32:28 +0800

    Update qwen3_tts_code2wav.py
    Signed-off-by: tanhaoan333 <tanhaoan@huawei.com>

--- vllm_omni/model_executor/models/qwen3_tts/qwen3_tts_code2wav.py
+++ vllm_omni/model_executor/models/qwen3_tts/qwen3_tts_code2wav.py
@@ -30,6 +30,7 @@ _REF_CONTEXT_CACHE_MAX_BYTES = 64 * 1024 * 1024
 torch.npu.config.allow_internal_format = False
 torch.npu.set_compile_mode(jit_compile=False)
 
+
 def _codec_ids_from_payload_or_input(
     input_ids: torch.Tensor,
     runtime_info: dict[str, Any] | None,
