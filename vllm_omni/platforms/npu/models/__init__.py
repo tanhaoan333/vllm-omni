02c63e590012a12c964d00fc8ede422f807c3b2f
tanhaoan333 <tanhaoan@huawei.com>
2026-06-03 16:32:27 +0800

    Update __init__.py
    Signed-off-by: tanhaoan333 <tanhaoan@huawei.com>

--- vllm_omni/platforms/npu/models/__init__.py
+++ vllm_omni/platforms/npu/models/__init__.py
@@ -1,2 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+_MODEL_PATCHES_APPLIED = False
+
+
+def apply_npu_model_patches() -> None:
+    """Register NPU-specific model monkey-patches (idempotent)."""
+    global _MODEL_PATCHES_APPLIED
+    if _MODEL_PATCHES_APPLIED:
+        return
+    from vllm_omni.platforms.npu.models.qwen3_tts_code2wav import apply_qwen3_tts_code2wav_patch
+
+    apply_qwen3_tts_code2wav_patch()
+    _MODEL_PATCHES_APPLIED = True
